71763c2b03eafc32dd1f544503d83b9dfa64c089
nakulnagariy <nagariya23nakul@gmail.com>
2018-07-15 23:20:16 +0530

    till testing setup done

---
+++ buildScripts/startMessage.js
@@ -0,0 +1,6 @@
+// var chalk = require('chalk'); //ES5 pattern
+
+import chalk from 'chalk';
+
+
+console.log(chalk.green('Starting app in dev mode...')); //eslint-disable-line no-console
---
+++ webpack.config.dev.js
@@ -0,0 +1,26 @@
+/**
+ *Note: Webpack won't actually generate any physical files for our development build. it will serve our build from memory. 
+ */
+import path from 'path';
+
+export default {
+  debug: true,
+  devtool: 'inline-source-map',
+  noInfo: false,
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loaders: ['style','css']}
+    ]
+  }
+}
